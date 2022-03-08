ca99da6c901cb8d3f47b3adb46ad0982036e0fbf
omar chatin <omerchatin19@gmail.com>
2022-03-08 14:56:58 +0300

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const nextTranslate = require("next-translate");
+
+module.exports = nextTranslate({
+  webpack: (config, { isServer, webpack }) => {
+    return config;
+  },
+});
