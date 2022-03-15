0a921476a8002b98089d9ae6d0836651de8974d4
omar chatin <omerchatin19@gmail.com>
2022-03-15 12:37:25 +0300

    a

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Footer from "./components/Footer";
-import Meta from "./components/Meta";
 import NavBar from "./components/NavBar";
 
 export default function Layout({ children }) {
   return (
     <>
-     
       <NavBar />
       {children}
       <Footer />
